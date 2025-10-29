b80076a9e20c65ee9ac666eff4188be722582132
devyalchemist <devyalchemist@gmail.com>
2025-10-29 20:36:07 +0100

    set to use aiven

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+import fs from "fs";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -7,8 +8,14 @@ dotenv.config();
 const pool = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
+	port: process.env.DB_PORT,
+
 	password: process.env.DB_PASS,
 	database: process.env.DB_NAME,
+	ssl: {
+		ca: process.env.DB_CA, // path to your certificate
+		rejectUnauthorized: true,
+	},
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0,
